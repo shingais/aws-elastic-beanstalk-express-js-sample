1efe81ea34f0cd0878873563fc9a49856cf5b932
shingais <129018269+shingais@users.noreply.github.com>
2023-04-16 17:52:01 +0930

    Update app.js
    git add app.js
    git commit -m "change message"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello GitHub!'));
+app.get('/', (req, res) => res.send('Hello GitHubs!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
